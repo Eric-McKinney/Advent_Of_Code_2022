265e30072cff296e0a95f86f35964a900e6b5ccd
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-04 13:47:44 -0500

    Done part 1

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,1 +1,24 @@
 
+with open("input.txt") as f:
+    data = f.readlines()
+
+items = [i for i in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+priorities = [p for p in range(1,53)]
+itemPriorities = dict(zip(items, priorities))
+
+sharedItems = []
+for line in data:
+    compartmentLength = (len(line) - 1) // 2
+    compartment1 = line[0:compartmentLength]
+    compartment2 = line[compartmentLength:]
+
+    for item in compartment1:
+        if item in compartment2:
+            sharedItems.append(item)
+            break
+
+sum_of_priorities = 0
+for item in sharedItems:
+    sum_of_priorities += itemPriorities[item]
+
+print(sum_of_priorities)
