f4d55590d44d77c525e9c1dd4d37a6a5ea80f4c9
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-05 11:34:12 -0500

    Finished part 2

--- Day4/day4.py
+++ Day4/day4.py
@@ -5,4 +5,17 @@ with open("input.txt", "r") as f:
 overlappingAssignments = 0
 for line in data:
     assignmentPair = line.split(",")
+    assignment1 = assignmentPair[0].split("-")
+    assignment2 = assignmentPair[1].split("-")
 
+    assign1_lower_limit = int(assignment1[0])
+    assign1_upper_limit = int(assignment1[1])
+
+    assign2_lower_limit = int(assignment2[0])
+    assign2_upper_limit = int(assignment2[1])
+
+    if (assign1_lower_limit <= assign2_upper_limit and assign1_upper_limit >= assign2_lower_limit) or \
+       (assign2_lower_limit <= assign1_upper_limit and assign2_upper_limit >= assign1_lower_limit):
+        overlappingAssignments += 1
+
+print(overlappingAssignments)
