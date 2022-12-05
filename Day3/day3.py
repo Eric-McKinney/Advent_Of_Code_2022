da6906c05d017d410ba8d066c7afa49ad0e6b012
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-05 09:24:01 -0500

    Finished day 3

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,24 +1,37 @@
 
-with open("input.txt") as f:
+with open("input.txt", "r") as f:
     data = f.readlines()
 
 items = [i for i in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 priorities = [p for p in range(1,53)]
 item_priorities = dict(zip(items, priorities))
 
-shared_items = []
-for line in data:
-    compartment_length = (len(line) - 1) // 2
-    compartment_1 = line[:compartment_length]
-    compartment_2 = line[compartment_length:]
 
-    for item in compartment_1:
-        if item in compartment_2:
-            shared_items.append(item)
-            break
+def find_common_item(elves: list) -> str:
+    elf1 = elves[0]
+    elf2 = elves[1]
+    elf3 = elves[2]
+    for item in elf1:
+        if item in elf2 and item in elf3:
+            return item
+
+
+badges = []
+elf_group = []
+for idx, line in enumerate(data):
+    match (idx % 3):
+        case 0 | 1:
+            elf_group.append(line)
+        case 2:
+            elf_group.append(line)
+            badge = find_common_item(elf_group)
+            badges.append(badge)
+            elf_group.clear()
+        case _:
+            print("Default case triggered somehow")
 
 sum_of_priorities = 0
-for item in shared_items:
-    sum_of_priorities += item_priorities[item]
+for badge in badges:
+    sum_of_priorities += item_priorities[badge]
 
 print(sum_of_priorities)
