ed19f994ab0b3e49db7aa29680ead34d97af9ac6
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-06 21:31:20 -0500

    Finished parsing

--- Day5/day5.py
+++ Day5/day5.py
@@ -2,26 +2,33 @@
 with open("input.txt", "r") as f:
     data = f.readlines()
 
-initial_crates_drawing = data[:8]
-stackNumbers = data[8]
+unparsed_crates_drawing = data[:8]
+stack_numbers = data[8]
 moves = data[10:]
 
 
 # getting indexes of each stack since number is aligned with stack
-stackNumberIndexes = {}
-numOfStacks = 0
-for idx, character in enumerate(stackNumbers):
+stack_number_indexes = {}
+num_of_stacks = 0
+for idx, character in enumerate(stack_numbers):
     if character.isdigit():
-        stackNumberIndexes[character] = idx
+        stack_number_indexes[idx] = int(character) - 1 # offset so it matches index
 
         # the last stack number is the amount of stacks
-        if len(stackNumbers) - (idx + 1) <= 2:
-            numOfStacks = int(character)
+        if len(stack_numbers) - (idx + 1) <= 2:
+            num_of_stacks = int(character)
 
 
-initial_crates = []
-for line in initial_crates_drawing:
+initial_stacks = []
+for i in range(num_of_stacks):
+    initial_stacks.append([])
+
+
+unparsed_crates_drawing.reverse() # starts adding to stacks from bottom up
+for line in unparsed_crates_drawing:
     for idx, character in enumerate(line):
-        if character.isalp():
-            pass
+        if character.isalpha():
+            stack_num = stack_number_indexes[idx]
+            initial_stacks[stack_num].append(character)
+
 
