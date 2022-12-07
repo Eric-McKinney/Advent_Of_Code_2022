ecd82731fb2cb768877eeeb79ae4a8c78dda3e79
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-06 19:15:35 -0500

    Middle of parsing hell

--- Day5/day5.py
+++ Day5/day5.py
@@ -2,12 +2,26 @@
 with open("input.txt", "r") as f:
     data = f.readlines()
 
-initial_crates = data[:8]
+initial_crates_drawing = data[:8]
+stackNumbers = data[8]
 moves = data[10:]
 
-initial_state = []
-for line in initial_crates:
-    crates = []
-    for character in line:
-        if character.isalpha():
-            crates.append(character)
+
+# getting indexes of each stack since number is aligned with stack
+stackNumberIndexes = {}
+numOfStacks = 0
+for idx, character in enumerate(stackNumbers):
+    if character.isdigit():
+        stackNumberIndexes[character] = idx
+
+        # the last stack number is the amount of stacks
+        if len(stackNumbers) - (idx + 1) <= 2:
+            numOfStacks = int(character)
+
+
+initial_crates = []
+for line in initial_crates_drawing:
+    for idx, character in enumerate(line):
+        if character.isalp():
+            pass
+
