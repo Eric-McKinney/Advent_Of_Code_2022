8404b685fe9b2e6081a5965daadcc2fa6f2f1af9
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-10 21:50:08 -0500

    Done part 2

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,13 +1,15 @@
 with open("input.txt", "r") as f:
     data = f.readline()
 
-for i in range(4, len(data)):
-    window = data[i-4: i]
+LENGTH_OF_WINDOW = 14
+
+for i in range(LENGTH_OF_WINDOW, len(data)):
+    window = data[i-LENGTH_OF_WINDOW: i]
 
     duplicate_checker = set()
     for character in window:
         duplicate_checker.add(character)
 
-    if len(duplicate_checker) == 4:
+    if len(duplicate_checker) == LENGTH_OF_WINDOW:
         print(i)
         break
