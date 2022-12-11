4e6426c05d0a52bf364d1579bff39135969375e2
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-10 23:25:06 -0500

    Finished main method, still making class

--- Day7/day7.py
+++ Day7/day7.py
@@ -1,18 +1,65 @@
+def path_to_string(path: list) -> str:
+    path_string = ""
+    for directory_name in path:
+        path_string += f"/{directory_name}"
+
+    return path_string
+
+
+class FileSystem:
+    def __init__(self):
+        self.file_system = {}
+        self.curr_directory = None
+        self.curr_directory_path = ""
+
+    def change_dir(self, directory_name: str) -> None:
+        path = self.curr_directory_path.split("/")
+
+        if directory_name == "..":
+            path.pop()
+            self.curr_directory_path = path_to_string(path)
+        elif directory_name not in path:
+            self.curr_directory_path += f"/{directory_name}"
+        else:
+            directory_idx = path.index(directory_name)
+            path = path[: directory_idx + 1]
+            self.curr_directory_path = path_to_string(path)
+
+    def add_dir(self, dir_name: str) -> None:
+        if dir_name not in self.file_system[self.curr_directory].keys():
+            self.file_system[self.curr_directory][dir_name] = {}
+
+    def add_file(self, file_name: str, file_size: int) -> None:
+        self.curr_directory[file_name] = file_size
+
+
 def main():
     with open("input.txt", "r") as f:
         data = f.readlines()
 
-    file_system = {}
+    file_system = FileSystem()
     for line in data:
-        if line[1] == "$":
-            line.split()
+        line.split()
+        if line[0] == "$":
             command = line[1]
 
             match command:
                 case "cd":
-                    directory = line[2]
+                    dir_name = line[1]
+                    file_system.change_dir(dir_name)
                 case "ls":
-                    pass
+                    continue
+                case _:
+                    print(f"Unhandled case: {command}\nFrom {line = }")
+                    break
+        else:
+            if line[0] == "dir":
+                dir_name = line[1]
+                file_system.add_dir(dir_name)
+            else:
+                file_size = int(line[0])
+                file_name = line[1]
+                file_system.add_file(file_name, file_size)
 
 
 if __name__ == "__main__":
