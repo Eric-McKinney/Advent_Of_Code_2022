bcd1656226c6ce70cee5335ef6a1fd650853ca9b
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-23 13:57:25 -0500

    Finished part 2

--- Day9/day9.py
+++ Day9/day9.py
@@ -5,7 +5,15 @@ class Rope:
         self.__head_y = 0
         self.__tail_x = 0
         self.__tail_y = 0
+
+        # part 2 stuff
+        self.rope_segments = []
+        for _ in range(9):
+            self.rope_segments.append([0, 0])
+        # end of part 2 stuff
+
         self.coordinates_tail_visited = {(0, 0)}
+        self.coordinates_tail_visited_part2 = {(0, 0)}
 
     def move_head(self, direction, num_of_moves):
         for _ in range(num_of_moves):
@@ -22,6 +30,7 @@ class Rope:
                     print(f"Direction '{direction}' not recognized")
 
             self.__move_tail()
+            self.__move_rope_segments()
 
     def __move_tail(self):
         x_diff = self.__head_x - self.__tail_x
@@ -37,6 +46,27 @@ class Rope:
             self.__tail_y += y_diff/abs(y_diff)
 
         self.coordinates_tail_visited.add((self.__tail_x, self.__tail_y))
+
+    def __move_rope_segments(self):
+        for idx, segment in enumerate(self.rope_segments):
+            if idx == 0:
+                x_diff = self.__head_x - segment[0]
+                y_diff = self.__head_y - segment[1]
+            else:
+                x_diff = self.rope_segments[idx - 1][0] - segment[0]
+                y_diff = self.rope_segments[idx - 1][1] - segment[1]
+
+            if abs(x_diff) <= 1 and abs(y_diff) <= 1:
+                return
+
+            if x_diff != 0:
+                segment[0] += x_diff/abs(x_diff)
+
+            if y_diff != 0:
+                segment[1] += y_diff / abs(y_diff)
+
+            if idx == 8:
+                self.coordinates_tail_visited_part2.add((segment[0], segment[1]))
 
 
 def part1():
@@ -56,5 +86,23 @@ def part1():
     print(num_coordinates_tail_visited)
 
 
+def part2():
+    with open("input.txt", "r") as f:
+        data = f.readlines()
+
+    rope = Rope()
+    for line in data:
+        line = line.split()
+
+        direction = line[0]
+        num_of_moves = int(line[1])
+
+        rope.move_head(direction, num_of_moves)
+
+    num_coordinates_tail_visited = len(rope.coordinates_tail_visited_part2)
+    print(num_coordinates_tail_visited)
+
+
 if __name__ == "__main__":
-    part1()
+    # part1()
+    part2()
