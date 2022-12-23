ae9c936e617df69add23b30473624126e9885e31
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-22 19:06:21 -0500

    Done with part 2

--- Day8/day8.py
+++ Day8/day8.py
@@ -39,7 +39,7 @@ def is_edge(grid: list, row: int, col: int) -> bool:
     return row == 0 or col == 0 or row == len(grid) - 1 or col == len(grid[row]) - 1
 
 
-def main():
+def part1():
     with open("input.txt", "r") as f:
         data = f.readlines()
 
@@ -59,7 +59,87 @@ def main():
     print(num_of_visible_trees)
 
 
+def calculate_scenic_score(trees_visible_left: int, trees_visible_right: int, trees_visible_top: int, trees_visible_bottom: int) -> int:
+    return trees_visible_left * trees_visible_right * trees_visible_top * trees_visible_bottom
+
+
+def count_visible_trees_left(grid: list, row: int, col: int, height: str) -> int:
+    visible_trees = 0
+
+    if col == 0:
+        return visible_trees
+
+    for tree_height in grid[row][col - 1: : -1]:
+        visible_trees += 1
+
+        if tree_height >= height:
+            return visible_trees
+
+    return visible_trees
+
+
+def count_visible_trees_right(grid:list, row: int, col: int, height: str) -> int:
+    visible_trees = 0
+    for tree_height in grid[row][col + 1: ]:
+        visible_trees += 1
+
+        if tree_height >= height:
+            return visible_trees
+
+    return visible_trees
+
+
+def count_visible_trees_top(grid:list, row: int, col: int, height: str) -> int:
+    visible_trees = 0
+
+    if row == 0:
+        return visible_trees
+
+    for trees in grid[row - 1: : -1]:
+        visible_trees += 1
+
+        tree_height = trees[col]
+        if tree_height >= height:
+            return visible_trees
+
+    return visible_trees
+
+
+def count_visible_trees_bottom(grid:list, row: int, col: int, height: str) -> int:
+    visible_trees = 0
+    for trees in grid[row + 1: ]:
+        visible_trees += 1
+
+        tree_height = trees[col]
+        if tree_height >= height:
+            return visible_trees
+
+    return visible_trees
+
+
+def part2():
+    with open("input.txt", "r") as f:
+        data = f.read()
+        data = data.strip()
+        data = data.split("\n")
+
+    tree_scenic_scores = {}
+
+    for row in range(len(data)):
+        for col in range(len(data[row])):
+            height = data[row][col]
+            trees_visible_left = count_visible_trees_left(data, row, col, height)
+            trees_visible_right = count_visible_trees_right(data, row, col, height)
+            trees_visible_top = count_visible_trees_top(data, row, col, height)
+            trees_visible_bottom = count_visible_trees_bottom(data, row, col, height)
+
+            scenic_score = calculate_scenic_score(trees_visible_left, trees_visible_right, trees_visible_top, trees_visible_bottom)
+            tree_scenic_scores[(row, col)] = scenic_score
+
+    max_scenic_score = max(tree_scenic_scores.values())
+    print(max_scenic_score)
 
 
 if __name__ == "__main__":
-    main()
+    part1()
+    part2()
