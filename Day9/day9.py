26bdbf574f8efb56e5349059b5eb4642cf3c7a68
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-23 13:29:25 -0500

    Finshed part 1

--- Day9/day9.py
+++ Day9/day9.py
@@ -1,28 +1,42 @@
 
 class Rope:
     def __init__(self):
-        self.__head = [0, 0]
-        self.__tail = [0, 0]
-        self.coordinates_tail_visited = {[0, 0]}
+        self.__head_x = 0
+        self.__head_y = 0
+        self.__tail_x = 0
+        self.__tail_y = 0
+        self.coordinates_tail_visited = {(0, 0)}
 
     def move_head(self, direction, num_of_moves):
         for _ in range(num_of_moves):
             match direction:
                 case "L":
-                    self.__head[0] -= 1
+                    self.__head_x -= 1
                 case "R":
-                    self.__head[0] += 1
+                    self.__head_x += 1
                 case "U":
-                    self.__head[1] += 1
+                    self.__head_y += 1
                 case "D":
-                    self.__head[1] -= 1
+                    self.__head_y -= 1
                 case _:
                     print(f"Direction '{direction}' not recognized")
 
             self.__move_tail()
 
     def __move_tail(self):
-        pass
+        x_diff = self.__head_x - self.__tail_x
+        y_diff = self.__head_y - self.__tail_y
+
+        if abs(x_diff) <= 1 and abs(y_diff) <= 1:
+            return
+
+        if x_diff != 0:
+            self.__tail_x += x_diff/abs(x_diff) # always 1, but keeps sign
+
+        if y_diff != 0:
+            self.__tail_y += y_diff/abs(y_diff)
+
+        self.coordinates_tail_visited.add((self.__tail_x, self.__tail_y))
 
 
 def part1():
@@ -34,9 +48,12 @@ def part1():
         line = line.split()
 
         direction = line[0]
-        num_of_moves = line[1]
+        num_of_moves = int(line[1])
 
         rope.move_head(direction, num_of_moves)
+
+    num_coordinates_tail_visited = len(rope.coordinates_tail_visited)
+    print(num_coordinates_tail_visited)
 
 
 if __name__ == "__main__":
