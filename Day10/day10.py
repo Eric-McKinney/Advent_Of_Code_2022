241ffeb3e161069a77d8148b27df29d6757aba31
Eric McKinney <ericmckinney.md@gmail.com>
2022-12-23 17:05:05 -0500

    Begun testing

---
+++ Day10/day10.py
@@ -0,0 +1,56 @@
+import sys
+
+
+class ClockCircuit:
+    def __init__(self):
+        self.clock_cycle = 0
+        self.x = 1
+        self.cycle_log = {0: 1}
+
+    def __update_clock(self):
+        self.clock_cycle += 1
+        self.cycle_log[self.clock_cycle] = self.x
+
+    def noop(self):
+        self.__update_clock()
+
+    def add_x(self, x: int):
+        self.__update_clock()
+
+        self.x += x
+        self.__update_clock()
+
+
+def calculate_signal_strength(circuit: ClockCircuit, cycle: int):
+    x = circuit.cycle_log[cycle]
+    return cycle * x
+
+
+def part1():
+    with open("input.txt", "r") as f:
+        data = f.readlines()
+
+    circuit = ClockCircuit()
+    for line in data:
+        line = line.split()
+
+        operation = line[0]
+
+        match operation:
+            case "noop":
+                circuit.noop()
+            case "addx":
+                x = int(line[1])
+                circuit.add_x(x)
+            case _:
+                print(f'Operation "{operation}" not recognized', file=sys.stderr)
+
+    signal_strength_sum = 0
+    for cycle in range(20, 260, 40): # gets 20th, 60th, 100th, ..., 220th cycles
+        signal_strength_sum += calculate_signal_strength(circuit, cycle)
+
+    print(signal_strength_sum)
+
+
+if __name__ == "__main__":
+    part1()
--- Day10/tests.py
+++ Day10/tests.py
@@ -1,8 +1,36 @@
+import sys
 import unittest
+import day10
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_part1(self):
+        with open("test_input.txt", "r") as f:
+            data = f.readlines()
+
+        circuit = day10.ClockCircuit()
+        for line in data:
+            line = line.split()
+
+            operation = line[0]
+
+            match operation:
+                case "noop":
+                    circuit.noop()
+                case "addx":
+                    x = int(line[1])
+                    circuit.add_x(x)
+                case _:
+                    print(f'Operation "{operation}" not recognized', file=sys.stderr)
+
+        signal_strength_sum = 0
+        for cycle in range(20, 260, 40):
+            signal_strength = day10.calculate_signal_strength(circuit, cycle)
+            signal_strength_sum += signal_strength
+            print(f"{cycle = }\n{signal_strength = }")
+
+        self.assertEqual(13140, signal_strength_sum)
+
 
 if __name__ == '__main__':
     unittest.main()
